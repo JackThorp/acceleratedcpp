739f3af2cbd49b5b36890fe002c8f3c4d2a7175c
Jack Thorp <jackthorp13@gmail.com>
2019-03-18 21:13:39 +0000

    Added alternatives into 6.4

--- chapter_6/6.3_copy_int_vector.cpp
+++ chapter_6/6.3_copy_int_vector.cpp
@@ -10,10 +10,31 @@ int main(int argc, char* argv[]) {
 
   vector<int> u(10, 100);
   vector<int> v;
+  vector<int> w;
+  // As v is uninitialised we cannot pass v.begin() to copy
+  // as copy will try to write to an unreserved location
+  
+  // Option 1
   copy(u.begin(), u.end(), back_inserter(v));
 
+  // Resize works but reserve does not
+  // maybe as reserve doesn't change vector size so can't be accessed. !
+  // Option 2 is probably preferable here if we know how big w
+  // will grow by as it reduces the number of allocate calls to resize
+  // that might occur from back_insertion.
+
+  // Option 2
+  w.resize(10);
+  copy(u.begin(), u.end(), w.begin());
+
+  // Print option 1
   ostream_iterator<int> out_it (cout, ", ");
   copy(v.begin(), v.end(), out_it);
+  
+  cout << endl;
+
+  // Print option 2
+  copy(w.begin(), w.end(), out_it);
   
   cout << endl;
 
